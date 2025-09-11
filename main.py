603eed5c2026794b043055ecfe6207de09cee11c
Ash Anwar <sparcogaming@gmail.com>
2025-09-11 14:44:09 +1000

    modified main and setup constants

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+from constants import *
+
+
 def main():
-    print("Hello from asteroids!")
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
