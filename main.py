975cc7c903be71dc66549da3a3f13fe5a38555c8
Ash Anwar <sparcogaming@gmail.com>
2025-09-11 19:02:44 +1000

    added fps and updated comments

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, "black")
+        screen.fill("black")
+        # updates the display every iteration
         pygame.display.flip()
+
+        # limits fps to 60
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
